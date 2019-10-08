744729055bcc9ab1f15217041181eb128100ae63
Alexander <shvetcov2000@gmail.com>
2019-10-08 16:32:50 +0300

    fix test errors in methods

--- enumerables.rb
+++ enumerables.rb
@@ -2,6 +2,8 @@
 
 module Enumerable
   def my_each
+    return to_enum(:my_each) unless block_given?
+
     i = 0
     while i < size
       yield(self[i])
@@ -11,6 +13,8 @@ module Enumerable
   end
 
   def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
     i = 0
     while i < size
       yield(self[i], i)
@@ -20,41 +24,63 @@ module Enumerable
   end
 
   def my_select
+    return to_enum(:my_select) unless block_given?
+
     arr = []
     my_each { |e| arr << e if yield(e) }
     arr
   end
 
-  def my_all?
-    my_each { |e| return false unless yield(e) }
+  def my_all?(pattern = nil)
+    my_each { |e| return false unless yield(e) } if block_given?
+    my_each { |e| return false unless e.class == pattern } if pattern.class == Class
+    my_each { |e| return false unless e =~ pattern } if pattern.class == Regexp
+    my_each { |e| return false unless e } unless pattern
     true
   end
 
-  def my_any?
-    my_each { |e| return true if yield(e) }
+  def my_any?(pattern = nil)
+    my_each { |e| return true if yield(e) } if block_given?
+    my_each { |e| return true if e.class == pattern } if pattern.class == Class
+    my_each { |e| return true if e =~ pattern } if pattern.class == Regexp
+    my_each { |e| return true if e } unless pattern
     false
   end
 
-  def my_none?
-    my_each { |e| return false if yield(e) }
+  def my_none?(pattern = nil)
+    my_each { |e| return false if yield(e) } if block_given?
+    my_each { |e| return false if e.class == pattern } if pattern.class == Class
+    my_each { |e| return false if e =~ pattern } if pattern.class == Regexp
+    my_each { |e| return false if e } unless pattern
     true
   end
 
-  def my_count
+  def my_count(item = nil, &block)
     count = 0
-    my_each { count += 1 }
+    my_each { |e| count += 1 if block.call(e) } if block_given?
+    return count if block_given?
+
+    my_each { |e| count += 1 if e == item } if item
+    return count if item
+
+    my_each { count += 1 } if !item && !block_given?
     count
   end
 
-  def my_map(proc)
+  def my_map(&block)
     arr = []
-    proc ? my_each { |e| arr << proc.call(e) } : my_each { |e| arr << yield(e) }
+    my_each { |e| arr << block.call(e) } if block_given?
     arr
   end
 
-  def my_inject(initial = nil)
+  def my_inject(initial = nil, sym = nil, &block)
     acc = initial || first
-    my_each { |e| acc = yield(acc, e) }
+    acc = first if initial.class == Symbol
+    acc = initial if !initial.class == Symbol
+    acc -= acc
+    my_each { |e| acc = block.call(acc, e) } if block_given?
+    my_each { |e| acc = acc.send(sym, e) } if initial && sym
+    my_each { |e| acc = acc.send(initial, e) } if initial.class == Symbol
     acc
   end
 end
